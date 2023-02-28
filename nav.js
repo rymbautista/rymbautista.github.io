dcf97abc3607572e4caef8b1abb22897d5bc9be9
rymbautista <86502642+rymbautista@users.noreply.github.com>
2023-02-28 11:12:39 -0800

    Nav bar slay

---
+++ nav.js
@@ -0,0 +1,22 @@
+import { html, render } from "https://unpkg.com/lit-html?module";
+
+let navPages = [
+  { name: "Home", link: "/" },
+  { name: "Cheatsheet", link: "/cheatsheet/cheat.html" },
+  { name: "Gallery", link: "/gallery/gallery.html" },
+  { name: "MP0", link: "/MP0_Writeup/writeup.html" },
+  { name: "MP1", link: "/MP1_Writeup/writeup.html" },
+  { name: "MP2", link: "/MP2_Writeup/writeup.html" },
+  { name: "MP3", link: "/MP3_Writeup/writeup.html" },
+  { name: "Proposal", link: "/FinalProject/writeup.html" },
+];
+
+function makeNav(links) {
+  return html`<nav>
+    ${links.map((page) => {
+      return html`<a href="${page.link}">${page.name}</a>`;
+    })}
+  </nav>`;
+}
+
+render(makeNav(navPages), document.getElementById("nav-container"));
